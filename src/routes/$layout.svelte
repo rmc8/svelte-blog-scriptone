0ce55d353c6e452679be9346f8f7fb42b7d6169a
rmc8 <kmyashi@rmc-8.com>
2023-06-26 16:55:40 +0900

    [FIX] error page

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { error } from '$app/stores';
+    import Header from './components/Header.svelte';
+    import Footer from './components/Footer.svelte';
+</script>
+
+<svelte:head>
+	<title>Scriptone - Error</title>
+</svelte:head>
+
+<main>
+    <Header />
+    <svelte:body>
+        <slot />
+    </svelte:body>
+    {#if $error}
+        <div>
+            <h1>Error - {$error.status}</h1>
+            <p>{$error.message}</p>
+        </div>
+    {/if}
+    <Footer />
+</main>
