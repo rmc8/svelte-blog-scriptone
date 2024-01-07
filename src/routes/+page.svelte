f40947b350d8c59224264f5e3aaaa2d56495572a
rmc-8.com <kmprmc@gmail.com>
2024-01-07 23:53:34 +0900

    separate articles  template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Header from '../components/Header.svelte';
 	import Footer from '../components/Footer.svelte';
+	import Articles from '../components/Articles.svelte';
 	import type { PageData } from './$types';
 	import { beforeUpdate } from 'svelte';
 
@@ -33,52 +34,7 @@
 	<section id="articles">
 		<ul>
 			{#each data.contents as content}
-				<li>
-					<div class="eyecatch_section">
-						<a href="/{content.id}">
-							<!-- <img src={content.eyecatch?.url} alt="eyecatch of {content.title}" /> -->
-							<img src={content.eyecatch} alt="eyecatch of {content.title}" />
-						</a>
-					</div>
-					<div class="article_info">
-						<div class="article_link"><a href="/{content.id}">{content.title}</a></div>
-						<div class="upper" style="display:flex">
-							<div class="category">
-								<a class="category_link" href="/category/{content.category.id}/p/1" rel="external"
-									>{content.category.name}</a
-								>
-							</div>
-							<div class="tags">
-								{#each content.tags as tag}
-									<div class="tag">
-										<div class="tag_icon">
-											<img src="/logo/tag.svg" width="20" height="20" alt="tag_icon" />
-										</div>
-										<div class="tag_link">
-											<a href="/tag/{tag.id}/p/1">
-												{tag.name}
-											</a>
-										</div>
-									</div>
-								{/each}
-							</div>
-						</div>
-						<div class="clock">
-							<img src="/clock.webp" width="20" height="20" alt="clock_icon" />
-						</div>
-						<time style="color:#666">
-							{new Date(content.createdAt)
-								.toLocaleString('ja-JP', {
-									year: 'numeric',
-									month: '2-digit',
-									day: '2-digit',
-									hour: '2-digit',
-									minute: '2-digit'
-								})
-								.replace(/\//g, '-')}</time
-						>
-					</div>
-				</li>
+				<Articles {content} />
 			{/each}
 		</ul>
 		<div class="pagination">
