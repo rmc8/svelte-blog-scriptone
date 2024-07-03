079174f925548dd7bd42a3d644d9f95961f74f57
rmc-8.com <kmprmc@gmail.com>
2024-07-03 20:57:10 +0900

    Headerの追加

--- src/routes/ToC1Kwin/multi_live_score_up_multiplier/+page.svelte
+++ src/routes/ToC1Kwin/multi_live_score_up_multiplier/+page.svelte
@@ -10,13 +10,38 @@
 	let activeTab = 'table_contents'; // 初期表示タブを設定
 </script>
 
+<svelte:head>
+	<title>Scriptone - みんなでライブ用スコアアップ倍率確認/title>
+	<meta name="description" content="みんなでライブ向けのスコアアップ倍率（実行値）の計算をしたり、表による実行値の表示をしたりします。" />
+	<meta name="twitter:site" content="@mskydev" />
+	<meta name="twitter:creator" content="@mskydev" />
+	<meta property="og:title" content="みんなでライブ用スコアアップ倍率確認" />
+	<meta name="twitter:title" content="みんなでライブ用スコアアップ倍率確認" />
+	<meta
+		property="og:description"
+		content="みんなでライブ向けのスコアアップ倍率（実行値）の計算をしたり、表による実行値の表示を行ったりします。"
+	/>
+	<meta
+		name="twitter:description"
+		content="みんなでライブ向けのスコアアップ倍率（実行値）の計算をしたり、表による実行値の表示を行ったりします。"
+	/>
+	<meta
+		name="twitter:image"
+		content="https://raw.githubusercontent.com/rmc8/ToC1Kwin/main/docs/assets/icon/eyecatch.jpg"
+	/>
+	<meta
+		property="og:image"
+		content="https://raw.githubusercontent.com/rmc8/ToC1Kwin/main/docs/assets/icon/eyecatch.jpg"
+	/>
+</svelte:head>
+
 <Header />
 <main>
 	<article>
 		<div class="container">
 			<h2>みんなでライブ用スコアアップ倍率確認</h2>
 			<p>
-				みんなでライブ向けのスコアアップ倍率（実行値）の計算をしたり、表による実行値の表示を行ったりします。
+				みんなでライブ向けのスコアアップ倍率（実行値）の計算をしたり、表による実行値の表示をしたりします。
 			</p>
 
 			<div class="py-4">
