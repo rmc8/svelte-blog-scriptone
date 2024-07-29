973eb829d5f576f3b11969bc14c953307616a888
rmc-8.com <kmprmc@gmail.com>
2024-07-30 00:30:10 +0900

    リダイレクトに伴うURL修正

--- src/components/ProsekaProfile.svelte
+++ src/components/ProsekaProfile.svelte
@@ -40,7 +40,8 @@
 <section id="about" class="mt-8">
 	<p class="text-center text-lg">
 		Kと申します。プロセカでおまかせしたり連勝したりすることが好きです。イベランは４位・５位・その他TOP1000↑が40個ほどあり、チアフル連勝は1120連勝しています。普段はとてもゆるい周回をしているので、スタンプ押しながらまったりMV鑑賞や特攻などなんでもご一緒できましたらうれしいです！{#if profile_link_enable}プロセカの詳細なプロフィールはURLからご覧ください。URL:
-			<a href="https://rmc-8.com/ToC1Kwin" class="hover:underline">https://rmc-8.com/ToC1Kwin</a
+			<a href="https://rmc-8.com/project_sekai" class="hover:underline"
+				>https://rmc-8.com/project_sekai</a
 			>{/if}
 	</p>
 </section>
