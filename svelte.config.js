9c36f44196348c9c39d19de63f161a8c9cf58906
rmc-8.com <kmprmc@gmail.com>
2024-08-10 15:26:44 +0900

    add entries

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		prerender: {
+			entries: ['*'],
 			handleHttpError: ({ path, referrer, message }) => {
 				console.warn(`${path} referred from ${referrer}: ${message}`);
 				return false;
